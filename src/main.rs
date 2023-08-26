437589769e4bce4002b2d6f5b17368149960ee01
darknet-lord <118698244+darknet-lord@users.noreply.github.com>
2023-08-26 10:38:32 +0100

    Add threading

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,39 @@ use std::{fs::File, io::{BufReader, BufRead, Read, Seek}};
 use walkdir::{WalkDir, Error};
 use clap::{Command, Arg};
 use regex::{Regex};
+use crossbeam_channel::unbounded;
+use crossbeam_utils::thread::scope;
+
 mod patterns;
 
+const WORKERS_AMOUNT: usize = 8;
+const MAXIMUM_TEXT_LENGTH: usize = 100;
 
-fn find_patterns(p: &patterns::Pattern, lines: std::io::Lines<&mut BufReader<&mut File>>) {
+struct SearchResult {
+    path: String,
+    lineno: usize,
+    pattern_id: u32,
+    text: String,
+
+}
+
+fn find_patterns(
+    path: &String, p: &patterns::Pattern, lines: std::io::Lines<&mut BufReader<&mut File>>, results: &mut Vec<SearchResult>
+) {
     let re = Regex::new(p.pattern).unwrap();
-    for (lineno, line) in lines.enumerate() {
+    let pth = path.clone();
+    for (lineidx, line) in lines.enumerate() {
         match line {
             Ok(l) => {
                 if let Some(_) = re.captures(&l.to_lowercase()) {
-                    println!("FOUND {}: line={}, description={}, text={}", p.name, lineno, p.description, l)
+                    let mut text = l.to_ascii_lowercase();
+                    if text.len() > MAXIMUM_TEXT_LENGTH {
+                        text.truncate(MAXIMUM_TEXT_LENGTH);
+                    }
+                    let result = SearchResult{
+                        path: (*pth).to_string(), pattern_id: p.id, lineno: lineidx + 1, text: text
+                    };
+                    results.push(result);
                 }
             },
             _ => {}
@@ -19,13 +42,14 @@ fn find_patterns(p: &patterns::Pattern, lines: std::io::Lines<&mut BufReader<&mu
     }
 }
 
-fn scan_file(path: &str) {
+fn scan_file(path: &String) -> Vec<SearchResult> {
     let file = File::open(path);
+    let mut results = vec![];
     match file {
         Ok(mut f) => {
             for p in patterns::PYTHON_SUB_PATTERNS {
                 let mut reader = BufReader::new(f.by_ref());
-                find_patterns(p, reader.by_ref().lines());
+                find_patterns(path, p, reader.by_ref().lines(), &mut results);
                 reader.seek(std::io::SeekFrom::Start(0)).unwrap();
             }
         }
@@ -33,21 +57,58 @@ fn scan_file(path: &str) {
             println!("ERROR: {}", err)
         }
     }
+    return results;
 }
 
 fn scan_files(target_dir: &String) {
-    for entry in WalkDir::new(target_dir) {
-        match entry {
-            Ok(e) => {
-                let path = e.path().as_os_str().to_str().unwrap();
-                if path.ends_with(".py") {
-                    scan_file(path);
+    let (fsnd, frcv) = unbounded();
+    let (wsnd, wrcv) = unbounded::<SearchResult>();
+
+    scope(|scope| {
+        scope.spawn(|_| {
+            loop {
+                match wrcv.recv() {
+                    Ok(result) => {
+                        println!("{} found. File: {}, line: {}, text: {}",
+                        result.pattern_id, result.path, result.lineno, result.text)
+                    },
+                    Err(_) => break,
                 }
-            },
-            _ => {}
+            }
+        });
+
+        for _ in 0..WORKERS_AMOUNT {
+            let wsnd = wsnd.clone();
+            scope.spawn(|_| {
+                loop {
+                    match frcv.recv() {
+                        Ok(path) => {
+                            for res in scan_file(&path) {
+                                wsnd.send(res).unwrap();
+                            }
+                        },
+                        Err(_) => break,
+                    }
+                }
+                drop(wsnd);
+            });
         }
-        
-    }
+        drop(wsnd);
+
+        for entry in WalkDir::new(target_dir) {
+            match entry {
+                Ok(e) => {
+                    let path = String::from(e.path().as_os_str().to_str().unwrap());
+                    if path.ends_with(".py") {
+                        fsnd.send(path).unwrap();
+                    }
+                },
+                _ => (),
+            }
+        }
+        drop(fsnd);
+    }).unwrap();
+    
 }
 
 fn main() -> Result<(), Error> {
--- src/patterns.rs
+++ src/patterns.rs
@@ -1,13 +1,13 @@
 
 pub struct Pattern {
-    pub name: &'static str,
+    pub id: u32,
     pub pattern: &'static str,
     pub description: &'static str,
 }
 
 pub static PYTHON_SUB_PATTERNS: &[Pattern; 4] = &[
-    Pattern{name: "django_settings", pattern: r"^.*django_settings_module.*", description: "django settings path"},
-    Pattern{name: "django_secret", pattern: "^.*django_secret_key.*$", description: "django secret key"},
-    Pattern{name: "password", pattern: "^.*password.*$", description: "password found"},
-    Pattern{name: "secret", pattern: "^.*secret.*$", description: "secret found"},
+    Pattern{id: 1, pattern: r"^.*django_settings_module.*", description: "django settings path"},
+    Pattern{id: 2, pattern: r"^.*django_secret_key.*$", description: "django secret key"},
+    Pattern{id: 3, pattern: r"^.*password.*$", description: "password found"},
+    Pattern{id: 4, pattern: r"^.*secret.*$", description: "secret found"},
 ];
