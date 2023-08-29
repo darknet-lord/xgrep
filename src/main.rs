66efa9439eddc85be1e15f48db72081f987a47a8
darknet-lord <118698244+darknet-lord@users.noreply.github.com>
2023-08-29 01:57:31 +0100

    Add arguments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 use std::{fs::File, io::{BufReader, BufRead, Read, Seek}};
 use walkdir::{WalkDir, Error};
-use clap::{Command, Arg};
-use regex::{Regex};
+use clap::{Command, Arg, value_parser};
+use regex::Regex;
 use crossbeam_channel::unbounded;
 use crossbeam_utils::thread::scope;
 
 mod patterns;
-
-const WORKERS_AMOUNT: usize = 8;
-const MAXIMUM_TEXT_LENGTH: usize = 100;
 
 struct SearchResult {
     path: String,
@@ -19,7 +16,8 @@ struct SearchResult {
 }
 
 fn find_patterns(
-    path: &String, p: &patterns::Pattern, lines: std::io::Lines<&mut BufReader<&mut File>>, results: &mut Vec<SearchResult>
+    path: &String, p: &patterns::Pattern, lines: std::io::Lines<&mut BufReader<&mut File>>, results: &mut Vec<SearchResult>,
+    max_text_len: usize
 ) {
     let re = Regex::new(p.pattern).unwrap();
     let pth = path.clone();
@@ -28,8 +26,8 @@ fn find_patterns(
             Ok(l) => {
                 if let Some(_) = re.captures(&l.to_lowercase()) {
                     let mut text = l.to_ascii_lowercase();
-                    if text.len() > MAXIMUM_TEXT_LENGTH {
-                        text.truncate(MAXIMUM_TEXT_LENGTH);
+                    if text.len() > max_text_len {
+                        text.truncate(max_text_len);
                     }
                     let result = SearchResult{
                         path: (*pth).to_string(), pattern_id: p.id, lineno: lineidx + 1, text: text
@@ -42,14 +40,14 @@ fn find_patterns(
     }
 }
 
-fn scan_file(path: &String) -> Vec<SearchResult> {
+fn scan_file(path: &String, max_text_len: usize) -> Vec<SearchResult> {
     let file = File::open(path);
     let mut results = vec![];
     match file {
         Ok(mut f) => {
             for p in patterns::PYTHON_SUB_PATTERNS {
                 let mut reader = BufReader::new(f.by_ref());
-                find_patterns(path, p, reader.by_ref().lines(), &mut results);
+                find_patterns(path, p, reader.by_ref().lines(), &mut results, max_text_len);
                 reader.seek(std::io::SeekFrom::Start(0)).unwrap();
             }
         }
@@ -60,7 +58,7 @@ fn scan_file(path: &String) -> Vec<SearchResult> {
     return results;
 }
 
-fn scan_files(target_dir: &String) {
+fn scan_files(target_dir: &String, worker_amount: u16, max_text_len: usize) {
     let (fsnd, frcv) = unbounded();
     let (wsnd, wrcv) = unbounded::<SearchResult>();
 
@@ -77,13 +75,13 @@ fn scan_files(target_dir: &String) {
             }
         });
 
-        for _ in 0..WORKERS_AMOUNT {
+        for _ in 0..worker_amount {
             let wsnd = wsnd.clone();
             scope.spawn(|_| {
                 loop {
                     match frcv.recv() {
                         Ok(path) => {
-                            for res in scan_file(&path) {
+                            for res in scan_file(&path, max_text_len) {
                                 wsnd.send(res).unwrap();
                             }
                         },
@@ -113,10 +111,16 @@ fn scan_files(target_dir: &String) {
 
 fn main() -> Result<(), Error> {
     let matches = Command::new("xgrep")
-        .arg(Arg::new("target-dir").long("target-dir").required(true)
-        ).get_matches();
+        .arg(Arg::new("target").long("target-dir").required(true))
+        .arg(Arg::new("workers").long("workers-amount").help("amount of parallel workers")
+            .value_parser(value_parser!(u16)).default_value("8"))
+        .arg(Arg::new("max-len").long("maximum-text-length").help("maximum text length")
+            .value_parser(value_parser!(usize)).default_value("100"))
+        .get_matches();
+    let worker_amount = *matches.get_one::<u16>("workers").unwrap();
+    let max_text_len = *matches.get_one::<usize>("max-len").unwrap();
 
-    let target_dir = matches.get_one::<String>("target-dir").expect("required");
-    scan_files(target_dir);
+    let target_dir = matches.get_one::<String>("target").expect("required");
+    scan_files(target_dir, worker_amount, max_text_len);
     return Ok(())
 }
